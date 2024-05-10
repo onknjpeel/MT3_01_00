63c87ccd937b40d1eb5abb0158e5927c6a49b7ae
onknjpeel <k023g0103@g.neec.ac.jp>
2024-05-10 15:53:57 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,120 @@
+#include <Novice.h>
+#include <cmath>
+
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+
+struct Matrix4x4 {
+	float m[4][4];
+};
+
+float cot(float x) {
+	float cot;
+	cot = 1 / std::tan(x);
+	return cot;
+}
+
+Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip) {
+	Matrix4x4 result;
+	result = {
+		(1 / aspectRatio) * cot(fovY / 2),0,0,0,
+		0,cot(fovY / 2),0,0,
+		0,0,farClip / (farClip - nearClip),1,
+		0,0,(-nearClip * farClip) / (farClip - nearClip),0
+	};
+	return result;
+}
+
+Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip) {
+	Matrix4x4 result;
+	result = {
+		2 / (right - left),0,0,0,
+		0,2 / (top - bottom),0,0,
+		0,0,1 / (farClip - nearClip),0,
+		(left + right) / (left - right),(top + bottom) / (top - bottom),nearClip / (nearClip - farClip),1
+	};
+	return result;
+}
+
+Matrix4x4 MakeVieportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth) {
+	Matrix4x4 result;
+	result = {
+		width / 2,0,0,0,
+		0,-height / 2,0,0,
+		0,0,maxDepth - minDepth,0,
+		left + (width / 2),top + (height / 2),minDepth,1
+	};
+	return result;
+}
+
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* string) {
+	Novice::ScreenPrintf(x, y, "%s", string);
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(x + column * kColumnWidth, y + row * kRowHeight + kRowHeight, "%6.02f", matrix.m[row][column]);
+		}
+	}
+}
+
+const char kWindowTitle[] = "LC1B_06_オノ_ケンジピエール_タイトル";
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	// ライブラリの初期化
+	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	Matrix4x4 orthographicMatrix = MakeOrthographicMatrix(-160.0f, 160.0f, 200.0f, 300.0f, 0.0f, 1000.0f);
+
+	Matrix4x4 perspectiveFovMatrix = MakePerspectiveFovMatrix(0.63f, 1.33f, 0.1f, 1000.0f);
+
+	Matrix4x4 viewportMatrix = MakeVieportMatrix(100.0f, 200.0f, 600.0f, 300.0f, 0.0f, 1.0f);
+
+	// キー入力結果を受け取る箱
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
+
+	// ウィンドウの×ボタンが押されるまでループ
+	while (Novice::ProcessMessage() == 0) {
+		// フレームの開始
+		Novice::BeginFrame();
+
+		// キー入力を受け取る
+		memcpy(preKeys, keys, 256);
+		Novice::GetHitKeyStateAll(keys);
+
+		///
+		/// ↓更新処理ここから
+		///
+
+		///
+		/// ↑更新処理ここまで
+		///
+
+		///
+		/// ↓描画処理ここから
+		///
+
+		MatrixScreenPrintf(0, 0, orthographicMatrix, "orthographicMatrix");
+
+		MatrixScreenPrintf(0, kRowHeight * 5, perspectiveFovMatrix, "perspectiveFovMatrix");
+
+		MatrixScreenPrintf(0, kRowHeight * 10, viewportMatrix, "viewportMatrix");
+
+		///
+		/// ↑描画処理ここまで
+		///
+
+		// フレームの終了
+		Novice::EndFrame();
+
+		// ESCキーが押されたらループを抜ける
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+			break;
+		}
+	}
+
+	// ライブラリの終了
+	Novice::Finalize();
+	return 0;
+}
